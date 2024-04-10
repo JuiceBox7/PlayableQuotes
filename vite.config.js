966c29b19f92d954ca79da3ab9a1a057b9e10d02
Julian Lara <juiceinbox7@gmail.com>
2024-04-09 18:34:38 -0700

    fix, hopefully

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+    base: "https://juicebox7.github.io/RotatingTriangle/",
     server: {
         watch: {
             usePolling: true,
