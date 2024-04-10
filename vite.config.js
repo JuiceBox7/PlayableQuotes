82dcff7d7fa25f1d4424e588e32affd43b3519f7
Julian Lara <juiceinbox7@gmail.com>
2024-04-09 18:43:15 -0700

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-    base: process.env.REPO_NAME || "/repo-name/",
+    base: "/RotatingTriangle/",
     server: {
         watch: {
             usePolling: true,
